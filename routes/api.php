f7308c8b56f5d2ff961501da13621d0f11604e43
muhammadshiva <shivamuhammad07@gmail.com>
2023-07-29 16:58:49 +0700

    create routes api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\DepartemenController;
+use App\Http\Controllers\DosenController;
+use App\Http\Controllers\JadwalController;
+use App\Http\Controllers\KelasController;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\MatakuliahController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +23,39 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Dosen
+Route::get('/dosen', [DosenController::class, 'index']);
+Route::post('/create-dosen', [DosenController::class, 'create']);
+Route::post('/{id}/update-dosen', [DosenController::class, 'update']);
+Route::delete('/{id}/delete-dosen', [DosenController::class, 'delete']);
+
+// Matakuliah
+Route::get('/matakuliah', [MatakuliahController::class, 'index']);
+Route::post('/create-matakuliah', [MatakuliahController::class, 'create']);
+Route::post('/{id}/update-matakuliah', [MatakuliahController::class, 'update']);
+Route::delete('/{id}/delete-matakuliah', [MatakuliahController::class, 'delete']);
+
+// Mahasiswa
+Route::get('/mahasiswa', [MahasiswaController::class, 'index']);
+Route::post('/create-mahasiswa', [MahasiswaController::class, 'create']);
+Route::post('/{id}/update-mahasiswa', [MahasiswaController::class, 'update']);
+Route::delete('/{id}/delete-mahasiswa', [MahasiswaController::class, 'delete']);
+
+// Departemen
+Route::get('/departemen', [DepartemenController::class, 'index']);
+Route::post('/create-departemen', [DepartemenController::class, 'create']);
+Route::post('/{id}/update-departemen', [DepartemenController::class, 'update']);
+Route::delete('/{id}/delete-departemen', [DepartemenController::class, 'delete']);
+
+// Kelas
+Route::get('/kelas', [KelasController::class, 'index']);
+Route::post('/create-kelas', [KelasController::class, 'create']);
+Route::post('/{id}/update-kelas', [KelasController::class, 'update']);
+Route::delete('/{id}/delete-kelas', [KelasController::class, 'delete']);
+
+// Jadwal
+Route::get('/jadwal', [JadwalController::class, 'index']);
+Route::post('/create-jadwal', [JadwalController::class, 'create']);
+Route::post('/{id}/update-jadwal', [JadwalController::class, 'update']);
+Route::delete('/{id}/delete-jadwal', [JadwalController::class, 'delete']);
